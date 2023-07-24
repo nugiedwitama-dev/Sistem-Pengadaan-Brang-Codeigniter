08cf309895d7d8345bd81c6038e2a68265daa9b3
unknown <admin@idcr.co.id>
2023-07-24 12:14:35 +0700

    'first-commmit'

---
+++ application/controllers/Barangkeluar.php
@@ -0,0 +1,106 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+error_reporting(0);
+
+class Barangkeluar extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+
+        $this->load->model('Admin_model', 'admin');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = "Barang keluar";
+        $data['barangkeluar'] = $this->admin->getBarangkeluar();
+        $this->template->load('templates/dashboard', 'barang_keluar/data', $data);
+    }
+
+    private function _validasi()
+    {
+        $this->form_validation->set_rules('tanggal_keluar', 'Tanggal Keluar', 'required|trim');
+        $this->form_validation->set_rules('barang_id', 'Barang', 'required');
+
+        $input = $this->input->post('barang_id', true);
+        $stok = $this->admin->get('barang', ['id_barang' => $input])['stok'];
+        $stok_valid = $stok + 1;
+
+        $this->form_validation->set_rules(
+            'jumlah_keluar',
+            'Jumlah Keluar',
+            "required|trim|numeric|greater_than[0]|less_than[{$stok_valid}]",
+            [
+                'less_than' => "Jumlah Keluar tidak boleh lebih dari {$stok}"
+            ]
+        );
+    }
+
+    public function add()
+    {
+        $this->_validasi();
+        if ($this->form_validation->run() == false) {
+            $data['title'] = "Barang Keluar";
+            $data['barang'] = $this->admin->get('barang', null, ['stok >' => 0]);
+
+            // Mendapatkan dan men-generate kode transaksi barang keluar
+            $kode = 'T-BK-' . date('ymd');
+            $kode_terakhir = $this->admin->getMax('barang_keluar', 'id_barang_keluar', $kode);
+            $kode_tambah = substr($kode_terakhir, -5, 5);
+            $kode_tambah++;
+            $number = str_pad($kode_tambah, 5, '0', STR_PAD_LEFT);
+            $data['id_barang_keluar'] = $kode . $number;
+
+            $this->template->load('templates/dashboard', 'barang_keluar/add', $data);
+        } else {
+            $input = $this->input->post(null, true);
+            $insert = $this->admin->insert('barang_keluar', $input);
+
+            if ($insert) {
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.fonnte.com/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'target' => '082138668809',
+                    'message' => 'Ada transaksi barang KELUAR baru,'.' segera cek di https://sparepart.idcr.co.id/barangkeluar', 
+                    'delay' => '2', 
+                    'countryCode' => '62', //optional
+                ),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: vR6GidYJFKSIfq37+IXq' //change TOKEN to your actual token
+                ),
+                ));
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                echo $response; 
+                
+                redirect('laporan');
+            } else {
+                set_pesan('Opps ada kesalahan!');
+                redirect('barangkeluar/add');
+            }
+        }
+    }
+
+    public function delete($getId)
+    {
+        $id = encode_php_tags($getId);
+        if ($this->admin->delete('barang_keluar', 'id_barang_keluar', $id)) {
+            set_pesan('data berhasil dihapus.');
+        } else {
+            set_pesan('data gagal dihapus.', false);
+        }
+        redirect('barangkeluar');
+    }
+}
---
+++ application/controllers/Barangmasuk.php
@@ -0,0 +1,98 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+error_reporting(0);
+
+class Barangmasuk extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+
+        $this->load->model('Admin_model', 'admin');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = "Barang Masuk";
+        $data['barangmasuk'] = $this->admin->getBarangMasuk();
+        $this->template->load('templates/dashboard', 'barang_masuk/data', $data);
+    }
+
+    private function _validasi()
+    {
+        $this->form_validation->set_rules('tanggal_masuk', 'Tanggal Masuk', 'required|trim');
+        $this->form_validation->set_rules('supplier_id', 'Supplier', 'required');
+        $this->form_validation->set_rules('barang_id', 'Barang', 'required');
+        $this->form_validation->set_rules('jumlah_masuk', 'Jumlah Masuk', 'required|trim|numeric|greater_than[0]');
+    }
+
+    public function add()
+    {
+        $this->_validasi();
+        if ($this->form_validation->run() == false) {
+            $data['title'] = "Barang Masuk";
+            $data['supplier'] = $this->admin->get('supplier');
+            $data['barang'] = $this->admin->get('barang');
+
+            // Mendapatkan dan men-generate kode transaksi barang masuk
+            $kode = 'T-BM-' . date('ymd');
+            $kode_terakhir = $this->admin->getMax('barang_masuk', 'id_barang_masuk', $kode);
+            $kode_tambah = substr($kode_terakhir, -5, 5);
+            $kode_tambah++;
+            $number = str_pad($kode_tambah, 5, '0', STR_PAD_LEFT);
+            $data['id_barang_masuk'] = $kode . $number;
+            $kode_pesan = $kode . $number;
+
+            $this->template->load('templates/dashboard', 'barang_masuk/add', $data);
+        } else {
+            $input = $this->input->post(null, true);
+            $insert = $this->admin->insert('barang_masuk', $input);
+
+            if ($insert) {
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.fonnte.com/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'target' => '082138668809',
+                    'message' => 'Ada barang MASUK baru, segera cek di https://sparepart.idcr.co.id/sparepart/barangmasuk', 
+                    'delay' => '2', 
+                    'countryCode' => '62', //optional
+                ),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: vR6GidYJFKSIfq37+IXq' //change TOKEN to your actual token
+                ),
+                ));
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                echo $response; 
+                
+                redirect('barangmasuk');
+                   
+            } else {
+                set_pesan('Opps ada kesalahan!');
+                redirect('barangmasuk/add');
+            }
+        }
+    }
+
+    public function delete($getId)
+    {
+        $id = encode_php_tags($getId);
+        if ($this->admin->delete('barang_masuk', 'id_barang_masuk', $id)) {
+            set_pesan('data berhasil dihapus.');
+        } else {
+            set_pesan('data gagal dihapus.', false);
+        }
+        redirect('barangmasuk');
+    }
+}
